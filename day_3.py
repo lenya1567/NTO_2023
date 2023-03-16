f50bb6958281d2be685c12a4650fdd898dee4610
lenya1567 <55053376+lenya1567@users.noreply.github.com>
2023-03-16 18:53:42 +0300

    Update day_3.py

--- day_3.py
+++ day_3.py
@@ -14,19 +14,19 @@ rospy.init_node("prog")
 
 bridge = CvBridge()
 
-test_public = rospy.Publisher('test_topic', Image, queue_size=1)
-fireRange = ((0, 62, 0), (36, 255, 255))
-sides = []
+test_public = rospy.Publisher('test_topic', Image, queue_size=1) # Топик для проверки работоспособности компьютерного зрения для распознавания возгораний
+fireRange = ((0, 62, 0), (36, 255, 255)) # Цветовые границы для определения пожара
+sides = [] # Ненужная переменная (пока не используем) 
 
-publisher = rospy.Publisher("fires", MarkerArray, queue_size=1)
-markerArray = MarkerArray()
+publisher = rospy.Publisher("fires", MarkerArray, queue_size=1) # Топик для вывода положения пожара в визуализаторе, с помощью Ros3DJs 
+markerArray = MarkerArray() # Массив элементов для визуализатора
 
-get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
+get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry) # Топик для телеметрии
 
-def get_distance(x1, y1, z1, x2, y2, z2):
+def get_distance(x1, y1, z1, x2, y2, z2): # Расстояние между точками в 3D через пространственную теорему Пифагора
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
 
-def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='', auto_arm=False, tolerance=0.2):
+def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='', auto_arm=False, tolerance=0.2): # Полёт в точку с ожиданием
     navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
 
     while not rospy.is_shutdown():
@@ -35,15 +35,15 @@ def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='', auto_
             break
         rospy.sleep(0.2)
 
-fires = []
-def addFire(x, y):
+fires = [] # Массив пожаров
+def addFire(x, y): # Функция для добавления пожаров в массив, с учётом, что не засекли один и тот же пожар дважды 
     global fires, markerArray, publisher
     notNeed = False
-    for fr in fires:
+    for fr in fires: # Проверка, что не было пожара в радиусе 50 см
         if get_distance(fr[0], fr[1], 0, x, y, 0) < 0.5:
             notNeed = True
             break
-    if not notNeed:
+    if not notNeed: # Если не нашёл, то создаём объект, записываем в массив и отправляем в визуализатор
         marker = Marker()
         marker.header.frame_id = "map"
         marker.type = marker.SPHERE
@@ -63,14 +63,16 @@ def addFire(x, y):
         markerArray.markers.append(marker)
         publisher.publish(markerArray)
 
+        # Делаем запрос GET через функцию requests
         URL = "http://65.108.222.51/check_material"
         PARAMS = {'x':round(x, 2), 'y':round(y, 2)}
 
         r = requests.get(url = URL, params = PARAMS)
         d = r.text.rstrip("\"").lstrip("\"")
-
+        
+        # Анализ того, что прислал сервер
         fireClass = "NO"
-
+    
         if d in ["coal", "textiles", "plastics", "oil", "alcohol", "glycerine"]:
              print("Fire detect right:", d)
              fireClass = d
@@ -84,6 +86,8 @@ def addFire(x, y):
         return True
     return False
 
+# Тут была часть кода, которая была дважды скопирована из терминала RPI и удалена во время запуска при организаторах
+
 data = []
 def range_callback(msg):
     global data
@@ -91,11 +95,12 @@ def range_callback(msg):
     T = get_telemetry(frame_id = "aruco_map")
     x, y = T.x, T.y
     if len(data) == 0:
-        data.append((dista, x, y))
+        data.append((dista, x, y)) # Начало движения с измерением длины стены
     last = data[-1]
 
-    if abs(last[0] - dista) > 0.6:
-         print(last, last[1], last[2])
+    if abs(last[0] - dista) > 0.6: # Нашли угол стен
+         
+         print(last, last[1], last[2]) # Выводы отладочной информации
          print("Next wall", "wall length", get_distance(x, y, 0, last[1], last[2], 0))
          print("Next wall", "wall length", abs(last[0] - dista))
          data.append((dista, x, y))
@@ -107,12 +112,12 @@ def image_callback(data):
     x1, y1, z1 = T2.x, T2.y, T2.z
 
     img = bridge.imgmsg_to_cv2(data, 'bgr8')
-    H, W, _ = img.shape
-    imgNew = img[H // 4 : 3 * H // 4, W // 4 : 3 * W // 4]
-    imgHSV = cv2.cvtColor(imgNew, cv2.COLOR_BGR2HSV)
-    threshold = cv2.inRange(imgHSV, fireRange[0], fireRange[1])
-    fireContours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    fireContours = list(filter(lambda x: cv2.contourArea(x) > 600, fireContours))
+    H, W, _ = img
+    imgNew = img[H // 4 : 3 * H // 4, W // 4 : 3 * W // 4] # Изображение уменьшаем для видимости только под коптером
+    imgHSV = cv2.cvtColor(imgNew, cv2.COLOR_BGR2HSV) # Изображение переводится в HSV 
+    threshold = cv2.inRange(imgHSV, fireRange[0], fireRange[1]) # Преобразование в чёрно-белое изображение через ворота цветов
+    fireContours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # Поиск контуров
+    fireContours = list(filter(lambda x: cv2.contourArea(x) > 600, fireContours)) # Отсечение маленьких контуров
 
     i = 0
     for contour in fireContours:
@@ -144,80 +149,7 @@ land = rospy.ServiceProxy('land', Trigger)
 
 rospy.Subscriber('rangefinder/range', Range, range_callback)
 
-navigate(z=1, speed=1, frame_id='body', auto_arm=True)
-rospy.sleep(5)
-
-T = get_telemetry(frame_id = "aruco_map")
-
-        else:
-             print("Fire detect error", d)
-
-        fires += [(x, y, fireClass)]
-
-        #stream = os.system("curl -X \'GET\' 'http://65.108.222.51/check_material?x=" + str(round(x, 2)) + "&y=" + str(round(x, 2)) + "\' -H \'accept: application/json\'")
-        #output = stream.read().rstrip()
-        return True
-    return False
-
-data = []
-def range_callback(msg):
-    global data
-    dista = msg.range
-    T = get_telemetry(frame_id = "aruco_map")
-    x, y = T.x, T.y
-    if len(data) == 0:
-        data.append((dista, x, y))
-    last = data[-1]
-
-    if abs(last[0] - dista) > 0.6:
-         print(last, last[1], last[2])
-         print("Next wall", "wall length", get_distance(x, y, 0, last[1], last[2], 0))
-         print("Next wall", "wall length", abs(last[0] - dista))
-         data.append((dista, x, y))
-
-def image_callback(data):
-    global hsvGateMin, hsvGateMax, test_public, fires, publisher, markerArray
-    publisher.publish(markerArray)
-    T2 = get_telemetry(frame_id = "aruco_map")
-    x1, y1, z1 = T2.x, T2.y, T2.z
-
-    img = bridge.imgmsg_to_cv2(data, 'bgr8')
-    H, W, _ = img.shape
-    imgNew = img[H // 4 : 3 * H // 4, W // 4 : 3 * W // 4]
-    imgHSV = cv2.cvtColor(imgNew, cv2.COLOR_BGR2HSV)
-    threshold = cv2.inRange(imgHSV, fireRange[0], fireRange[1])
-    fireContours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    fireContours = list(filter(lambda x: cv2.contourArea(x) > 600, fireContours))
-
-    i = 0
-    for contour in fireContours:
-        i += 1
-        M = cv2.moments(contour)
-        cx = int(M["m10"] / M["m00"])
-        cy = int(M["m00"] / M["m00"])
-        cv2.drawContours(img, [contour], -1, (255, 0, 0), 5)
-
-        # T2 = get_telemetry(frame_id = "aruco_map")
-        #x1, y1, z1 = T.x, T.y, T.z
-        if addFire(x1, y1):
-            print([x1, y1]) #, get_telemetry(frame_id = "aruco_map"))
-
-    test_public.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
-
-image_sub = rospy.Subscriber('main_camera/image_raw_throttled', Image, image_callback)
-
-# rospy.init_node('flight')
-
-get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
-navigate = rospy.ServiceProxy('navigate', srv.Navigate)
-navigate_global = rospy.ServiceProxy('navigate_global', srv.NavigateGlobal)
-set_position = rospy.ServiceProxy('set_position', srv.SetPosition)
-set_velocity = rospy.ServiceProxy('set_velocity', srv.SetVelocity)
-set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
-set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
-land = rospy.ServiceProxy('land', Trigger)
-
-rospy.Subscriber('rangefinder/range', Range, range_callback)
+# Движение коптера по заданной траектории
 
 navigate(z=1, speed=1, frame_id='body', auto_arm=True)
 rospy.sleep(5)
@@ -260,13 +192,13 @@ for e in fires:
         findFires += [(e[0], e[1])]
         was += [(e[0], e[1])]
 '''
-print("Auto report:")
+print("Auto report:") # Вывод автоотчёта
 
 print()
 print("Fires:", len(fires))
 print()
 
-for i in range(len(fires)):
+for i in range(len(fires)): # Определение класса пожара и вывод координат и класса пожара
     name = fires[i][2]
     group = "undefined"
     if name in ["coal", "textiles", "plastics"]:
@@ -275,10 +207,10 @@ for i in range(len(fires)):
         group = "B"
     print("Fire", str(i + 1) + ":", round(fires[i][0], 2), round(fires[i][1], 2), name, group)
 
-print("Injured: 0")
+print("Injured: 0") # Количество найденных пострадавших
 
 c = 0
-for i in range(len(data)):
+for i in range(len(data)): # Вывод длин стен 
     c += 1
     print("Wall", str(c) + ":", get_distance(x, y, 0, last[1], last[2], 0))
 
